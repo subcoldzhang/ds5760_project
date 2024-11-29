acb841e253845e6f7875d902fa069b8d60c2e6d6
subcoldzhang <143550483+subcoldzhang@users.noreply.github.com>
2024-11-29 12:46:40 -0600

    Delete ds5760_project directory

--- ds5760_project/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-# __name__ is the package name - 'app', for locating templates and static files
-app = Flask(__name__)
-
-from app import MongoDB 
-from app import neo4j
--- ds5760_project/run-app.py
+++
@@ -1,9 +0,0 @@
-from app import app
-
-if __name__ == '__main__':
-    ''' 
-        Running app in debug mode
-        It will trace errors if produced and display them
-        Each time a change is made in code, the changes will reflect instantaneously. 
-    '''
-    app.run(debug=True)
